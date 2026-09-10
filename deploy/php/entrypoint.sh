56b9ce2dc30a44fafae81eb696103ff728dc2b16
rh64s <cchelovekov64@gmail.com>
2026-09-11 01:27:07 +0700

    nah

--- deploy/php/entrypoint.sh
+++ deploy/php/entrypoint.sh
@@ -5,7 +5,7 @@ chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
 cd /var/www
-#composer install --optimize-autoloader --no-interaction --no-progress
+composer install --optimize-autoloader --no-interaction --no-progress
 php artisan key:generate --force
 php artisan migrate:fresh --seed --force
 php artisan storage:link --force
