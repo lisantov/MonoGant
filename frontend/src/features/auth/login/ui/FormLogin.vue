0941ddb4a1d70e99183ec727f1fc9c247f765316
elfi <gugl6369@gmail.com>
2026-09-10 19:25:24 +0700

    fix

---
+++ frontend/src/features/auth/login/ui/FormLogin.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+// import {useForm} from "vee-validate";
+// import { loginSchema } from "../lib/validation.ts"
+
+// const { defineField, handleSubmit, errors, meta } = useForm({
+//   validationSchema: loginSchema,
+// })
+
+// const [login, loginAttrs] = defineField("login")
+// const [password, passwordAttrs] = defineField("password")
+
+// const onSubmit = handleSubmit((values) => {
+//   mutateAsync(values)
+// })
+</script>
+
+<template>
+  <form class="linear-border">
+    <h2>Авторизация</h2>
+    <div>
+      <input>
+      <input>
+    </div>
+    <div>
+      <div class="w-full">
+        <a
+          class="w-fit text-gray-100 text-sm transition duration-300 hover:text-primary-orange"
+        >
+          Забыли пароль?
+        </a>
+      </div>
+    </div>
+    <div>
+      <button />
+      <p
+        class="font-semibold text-primary-orange dark:text-gray-100 transition duration-300 hover:text-primary-orange"
+      >
+        Зарегистрироваться
+      </p>
+    </div>
+  </form>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/features/auth/register/ui/FormRegister.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+// import { registerSchema } from '@/features';
+// import {useForm} from "vee-validate";
+
+// const { defineField, handleSubmit, errors, meta } = useForm({
+//   validationSchema: registerSchema,
+// })
+
+// const { mutateAsync, isLoading } = useRegister()
+
+// const [username, usernameAttrs] = defineField("username")
+// const [email, emailAttrs] = defineField("email")
+// const [password, passwordAttrs] = defineField("password")
+// const [confirmPassword, confirmPasswordAttrs] = defineField("confirmPassword")
+
+// const onSubmit = handleSubmit((values) => {
+//   mutateAsync(values)
+// })
+</script>
+
+<template>
+  <div>
+    <form>
+      <label for="name">
+        <input>
+      </label>
+      <label for="email">
+        <input>
+      </label>
+      <label for="password">
+        <input>
+      </label>
+      <label for="confirmPassword">
+        <input>
+      </label>
+    </form>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
