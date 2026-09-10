62cfe6892685a3c6da243f5e2dadaa3d61714405
rh64s <cchelovekov64@gmail.com>
2026-09-11 01:37:05 +0700

    fix: profile

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,9 +8,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/user', [\App\Http\Controllers\UserController::class, 'profile'])->middleware('auth:sanctum');
 
 Route::get('/hi', [AuthController::class, 'index']);
 
