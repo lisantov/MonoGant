6c6c1308c8366fe4d06af74962cc7d0cfe355a57
rh64s <cchelovekov64@gmail.com>
2026-09-11 07:22:20 +0700

    feat

--- backend/app/Http/Resources/UserResource.php
+++ backend/app/Http/Resources/UserResource.php
@@ -14,6 +14,7 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+
         return [
             'name' => $this->name,
             'email' => $this->email,
