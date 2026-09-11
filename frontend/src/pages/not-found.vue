df5e3d9b9e40c812c2e113f67022c41b7c32b1b1
elfi <gugl6369@gmail.com>
2026-09-11 13:49:25 +0700

    fix: not-found.vue

--- frontend/src/pages/not-found.vue
+++ frontend/src/pages/not-found.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex w-full items-center gap-12">
-    <h1 class="text-[180px] font-medium">
+  <div class="flex flex-col min-h-[90vh] w-full items-center justify-center gap-10">
+    <h1 class="text-[280px] font-jost">
       404
     </h1>
     <div class="flex flex-col gap-4">
-      <span class="text-2xl">Кажется, вы заблудились</span>
+      <span class="text-2xl font-jost">Кажется, вы заблудились</span>
       <router-link :to="Routes.home">
         <app-button> На главную </app-button>
       </router-link>
@@ -17,3 +17,5 @@ import { Routes } from '@/shared';
 import { AppButton } from '@/shared/ui';
 import { RouterLink } from 'vue-router';
 </script>
+
+<style scoped></style>
